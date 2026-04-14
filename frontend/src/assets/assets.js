d51ca8746f42896ed41f13ea12e6c6ceb1dcb80a
KenzoSant <mateuskenzo_santos@hotmail.com>
2026-04-14 17:39:01 -0300

    assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,13 +1,11 @@
 import logo from "./img/logo.png";
 import shape from "./img/shape.png";
-import video from "./img/video.mp4"
 import video1 from "./img/video1.mp4"
 
 
 export const assets = {
   logo,
   shape,
-  video,
   video1
 };
 
